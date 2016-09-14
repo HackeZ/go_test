15e7d843d42946b25d5bcf265a36ebc0b4595d7f
HackeZ <767110505@qq.com>
2016-09-14 13:34:10 +0800

    add pool_test

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 It is a project that i can try some golang package or funny way.
 
 :)
+
+## Usage
+
+Beause It is not a Library. Use `git clone` instead of `go get` Plz.
---
+++ pool_test/pool.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"container/list"
+	"errors"
+	"log"
+)
+
+var (
+	ErrNoMoreObject = errors.New("No More Object in Pool")
+	ErrNoMoreSpace  = errors.New("No More Space in Pool")
+)
+
+// Object ..
+type Object struct {
+	ID int
+}
+
+// Pool a pool
+type Pool interface {
+	Borrow() (*Object, error)
+	Return(*Object) error
+}
+
+// Allocate return index to Object.
+type Allocate func(int) (*Object, error)
+
+type objectPool struct {
+	Size     int
+	Allocate Allocate
+	FreeList *list.List
+}
+
+func (p *objectPool) Borrow() (*Object, error) {
+	elem := p.FreeList.Front()
+	if elem == nil {
+		return nil, ErrNoMoreObject
+	}
+
+	obj := p.FreeList.Remove(elem)
+	o := obj.(*Object)
+
+	return o, nil
+}
+
+func (p *objectPool) Return(obj *Object) error {
+	if p.Size == p.FreeList.Len() {
+		return ErrNoMoreSpace
+	}
+
+	p.FreeList.PushBack(obj)
+	return nil
+}
+
+// New return a available Pool
+func New(initPoolSize int, alloc Allocate) (Pool, error) {
+	op := &objectPool{
+		Size:     initPoolSize,
+		Allocate: alloc,
+		FreeList: list.New(),
+	}
+
+	var obj *Object
+	var err error
+	for i := 0; i < initPoolSize; i++ {
+		obj, err = op.Allocate(i)
+		if err != nil {
+			return nil, err
+		}
+
+		op.FreeList.PushFront(obj)
+	}
+	return op, nil
+}
+
+func main() {
+	const poolSize = 3
+
+	p, _ := New(poolSize, func(i int) (*Object, error) {
+		return &Object{ID: i}, nil
+	})
+
+	// Borrow a Object to Test.
+	ob, err := p.Borrow()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("borrow a object from pool: %#v\n", *ob)
+
+	// Borrow All Object in Pool
+	for i := 0; i < poolSize-1; i++ {
+		o, err := p.Borrow()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("borrow a object from pool: %#v\n", *o)
+	}
+
+	p.Return(ob)
+	ob1, err := p.Borrow()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if ob != ob1 {
+		log.Fatal("expect the same object")
+	}
+}
